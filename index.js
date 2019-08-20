c1a0f911e9d89bfd8f18a9328ae013661823fcf1
Tunde Ajagbe <tundeajagbea@gmail.com>
2019-08-20 14:24:16 +0100

    Changes to be committed: 	modified:   index.js
    		 *Created a PersonalDetails class and instatiate it
    		 *Returned the created object using JSON.stringify

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+
+const port = 8081;
 const http = require('http');
 
-http.createServer((req, res) => {
+class PersonalDetails{
+    constructor(name, email, phone){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    get personNme(){
+        return this.name;
+    }
+
+    set personName(name){
+        this.name = name;
+    }
+
+    get personEmail(){
+        return this.email;
+    }
+
+    set personEmail(email){
+        this.email = email;
+    }
+
+    get personPhone(){
+        return this.phone;
+    }
+
+    set personPhone(phone){
+        this.phone = phone;
+    }
+
+}
+
+const person = new PersonalDetails("Tunde Ajagbe", "tundeajagbea@gmail.com", 08106887783);
+
+var serv = http.createServer((req, res) => {
     res.writeHead(200, {'content-Type': 'text/plain'});
-    res.write("Hello world #newbies");
+    res.write(JSON.stringify(person));
     res.end();
-}).listen(8081);
+});
+
+serv.listen(port);
+console.log(`listening on port ${port} ...`);
+
+
